cc1ddfc055d311d8a048f0ca1136095f4e80e5d6
Raymondacampo <raymondacamposandoval@gmail.com>
2024-02-18 00:16:38 -0400

    dropdowns

--- doctor/web/models.py
+++ doctor/web/models.py
@@ -6,8 +6,19 @@ class Ensurance(models.Model):
     name = models.CharField(max_length=200)
     plan = models.CharField(max_length=200)
 
+    def __str__(self):
+        return f"{self.name}, {self.plan}."
+
 class Speciality(models.Model):
     speciality = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.speciality}"
+    
+    def serialize(self):
+        return{
+            'speciality':self.speciality
+        }
 
 class Clinic(models.Model):
     name = models.CharField(max_length=200)
@@ -15,9 +26,15 @@ class Clinic(models.Model):
     ensurance = models.ManyToManyField(Ensurance, blank=True, related_name="clinics")
     contact = models.CharField(max_length=100)
 
+    def __str__(self):
+        return f"{self.name}"
+
 class Doctor(models.Model):
     name = models.CharField(max_length=200)
     speciality = models.ManyToManyField(Speciality, related_name="doctors")
     clinic = models.ManyToManyField(Clinic, blank=True,  related_name="doctors")
     availability = models.CharField(max_length=500)
     contact = models.CharField(max_length=500)
+
+    def __str__(self):
+        return f"{self.name}"
--- doctor/web/views.py
+++ doctor/web/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Speciality, Ensurance, Clinic
+from django import forms
+from .forms import SomeForm
 # Create your views here.
+
 def index(request):
-    return render(request, "index.html")
+    specialities = Speciality.objects.all()
+    ensurance = Ensurance.objects.all()
+    clinic = Clinic.objects.all()
+    return render(request, "web/index.html",{
+        'specialities':specialities,
+        'ensurance':ensurance,
+        'clinic':clinic
+    })
 
 def search(request, doctor):
-    return render(request, "index.html")
+    return render(request, "web/index.html")
